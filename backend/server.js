0bb09b28bdffdf8245e7b16059d59d38b0b624e2
Abhsihek <abhistr1905@gmai.com>
2024-10-04 22:33:12 +0530

    dist fonder add

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,8 @@ app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
-app.use(cookieParser());
-
+app.use(cookieParser()); 
+ 
 app.use("/api/auth",authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
