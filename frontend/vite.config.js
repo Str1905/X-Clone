a16b8d8eae542eb200f9ba9de5501797079f20ab
Abhsihek <abhistr1905@gmai.com>
2024-10-04 23:15:45 +0530

    Backend_url is modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import dotenv from "dotenv";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "http://localhost:8000",
+				target: process.env.Backend_URL,
 				changeOrigin: true, 
 			},
 		},
