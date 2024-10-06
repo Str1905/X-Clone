12c09a96796e1c9a9027921279e1119d39c8adc4
Abhsihek <abhistr1905@gmai.com>
2024-10-07 02:33:22 +0530

    Access-Control-Allow-Origin', process.env.frontend_URL is added

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -26,7 +26,7 @@ const LoginPage = () => {
 		mutationFn: async ({ username, password }) => {
 			const apiUrl = `${import.meta.env.VITE_Backend_URL}/api/auth/login`;
 			try {
-				console.log(apiUrl)
+				// console.log(apiUrl)
 				const res = await fetch(apiUrl, {
 					method: "POST",
 					headers: {
@@ -37,9 +37,9 @@ const LoginPage = () => {
 					credentials: "include",
 					body: JSON.stringify({ username, password }),
 				});
-				console.log("this res",res);
+				// console.log("this res",res);
 				const data = await res.json();
-				console.log(data)
+				// console.log(data)
 				if(data.error) {throw new Error(data.error)}
 				// if (!res.ok) {
 				// 	throw new Error(data.error || "Something went wrong");
